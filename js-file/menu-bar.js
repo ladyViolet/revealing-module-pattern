d63b76a535089020020b71746dfc533b7ca62fa4
MariaTheresiaDudyka <maria-theresia.dudyka@stud.uni-regensburg.de>
2019-03-25 00:32:22 +0100

    Update happy-colors.js
    Update menu bar

--- js-file/menu-bar.js
+++ js-file/menu-bar.js
@@ -1,9 +1,21 @@
 /*Js-Code für mobile devices*/
+$(window).on("scroll touchmove", function () {
+	$("header").toggleClass("tiny", $(document).scrollTop() > 0);
+	/*$("header nav ul li").toggleClass("tiny", $(document).scrollTop() > 0);*/
+	/*$("header nav ul").toggleClass("open", $(document).scrollTop() > 0);*/
+	$("#logo").toggleClass("tiny", $(document).scrollTop() > 0);
+	$(".fa-bars").toggleClass("tiny", $(document).scrollTop() > 0);
+});
+
 $(document).ready(function() {
 	$(".fa-bars").on("click", function() {
 		$("header nav ul").toggleClass("open");
 	});
 
+/*	$(".fa-bars.tiny").on("click", function() {
+		$("header nav ul li").toggleClass("open");
+	});
+*/
 	$("header nav ul li").on("click", function() { 
 		$("header nav ul").removeClass("open");
 	});
@@ -14,10 +26,4 @@ $(document).ready(function() {
 
 });
 
-$(window).on("scroll touchmove", function () {
-	$("header").toggleClass("tiny", $(document).scrollTop() > 0);
-	$("header nav ul li").toggleClass("tiny", $(document).scrollTop() > 0);
-	$("#logo").toggleClass("tiny", $(document).scrollTop() > 0);
-	$(".fa-bars").toggleClass("tiny", $(document).scrollTop() > 0);
-});
 
