d99f743ddbaae3885feb9e3a8e43af0fa7c66c52
MariaTheresiaDudyka <maria-theresia.dudyka@stud.uni-regensburg.de>
2019-03-25 01:39:14 +0100

    Update happy-colors.js, menu-bar.js
    Update header

--- js-file/menu-bar.js
+++ js-file/menu-bar.js
@@ -1,8 +1,7 @@
 /*Js-Code für mobile devices*/
 $(window).on("scroll touchmove", function () {
 	$("header").toggleClass("tiny", $(document).scrollTop() > 0);
-	/*$("header nav ul li").toggleClass("tiny", $(document).scrollTop() > 0);*/
-	/*$("header nav ul").toggleClass("open", $(document).scrollTop() > 0);*/
+	$("header nav ul li").toggleClass("tiny", $(document).scrollTop() > 0);
 	$("#logo").toggleClass("tiny", $(document).scrollTop() > 0);
 	$(".fa-bars").toggleClass("tiny", $(document).scrollTop() > 0);
 });
@@ -12,10 +11,10 @@ $(document).ready(function() {
 		$("header nav ul").toggleClass("open");
 	});
 
-/*	$(".fa-bars.tiny").on("click", function() {
-		$("header nav ul li").toggleClass("open");
+	$(".fa-bars.tiny").on("click", function() {
+		$("header nav ul").toggleClass("open");
 	});
-*/
+
 	$("header nav ul li").on("click", function() { 
 		$("header nav ul").removeClass("open");
 	});
